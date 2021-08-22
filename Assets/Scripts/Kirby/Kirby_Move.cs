a246b5ecc4807359e817bc88bc628ce14037563a
ChangMini <dlwlxns4@gmail.com>
2021-08-23 02:44:17 +0900

    feat: 대시 기능 구현

--- Assets/Scripts/Kirby/Kirby_Move.cs
+++ Assets/Scripts/Kirby/Kirby_Move.cs
@@ -9,15 +9,17 @@ public class Kirby_Move : MonoBehaviour
     public Animator animator;
     
     [Header("Move")]
-    public float speed = 4f;
+    public float speed = 3f;
+    float dashSpeed = 1f;
     SpriteRenderer spriteRenderer;
     
 
     [Header("Run")]
-    int canRun=0;    
-    float isRunTime=0.5f;
-    bool isDash = false;
 
+    bool canRun =false; // 달릴수 있는지 딜레이
+    bool isRight=false, isLeft = false;
+    float RunDelay=0.4f; // 더블클릭 시간 
+    
 
 
     
@@ -30,47 +32,93 @@ public class Kirby_Move : MonoBehaviour
         // StartCoroutine("DashCheck");
     }
 
-    IEnumerator DashCheck(){
-        while(true){
-            if(Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow)){
-                canRun++;
-                Debug.Log("True됐다");
-                Debug.Log(isRunTime);
-                animator.SetBool("isRun",false);            
-            }
 
-            if(canRun>=1 && !isDash){
-                isRunTime -=Time.deltaTime;// 제한시간 줄이기
-                if(isRunTime <= 0){ // 시간 다됐을 때 
-                    canRun = 0; // 달리는 판정 false
-                    isRunTime = 0.5f; //시간초 돌려주기
-                }
-            }
-            yield return null;
-        }
-    }
 
     // Update is called once per frame
-    void Update()
-    {      
-        // MoveCheck();
 
-        if( Input.GetKey(KeyCode.LeftArrow)){
-            rigid.AddForce( new Vector2(speed*-1, 0),ForceMode2D.Impulse);
-            if(rigid.velocity.x < -5f) 
-                rigid.velocity = new Vector2(-4f,rigid.velocity.y);
-            spriteRenderer.flipX=true;
-            animator.SetBool("isWalk", true);
-        }else if( Input.GetKey(KeyCode.RightArrow)){
-            rigid.AddForce( new Vector2(speed*1, 0),ForceMode2D.Impulse);
-            if(rigid.velocity.x >5f) 
-                rigid.velocity = new Vector2(4f,rigid.velocity.y);
-            spriteRenderer.flipX=false;
-            animator.SetBool("isWalk", true);
-        }else if(rigid.velocity.x == 0){
-            animator.SetBool("isWalk",false);
+    IEnumerator ActiveRunTimer(){
+        RunDelay=0.4f;
+        while(RunDelay >=0){
+            RunDelay -= Time.deltaTime;
+            yield return null;
         }
+        RunDelay=0.4f;
+    }
+    void Update()
+    {   
+        if(Input.GetKeyUp(KeyCode.RightArrow)){
+            StopCoroutine("ActiveRunTimer");
+            StartCoroutine("ActiveRunTimer");
+            isRight=true; //
+            isLeft=false; // 좌우 도리도리 했을때 바로 대쉬 되는 것 방지
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftArrow)){
+            StopCoroutine("ActiveRunTimer");
+            StartCoroutine("ActiveRunTimer");
+            isRight=false;
+            isLeft=true;
+        }
+        Debug.Log(RunDelay);
+
+        //왼쪽 이동시 로직
+        if( Input.GetKey(KeyCode.LeftArrow)){
+            if(RunDelay>0 && RunDelay<0.4f && rigid.velocity.x <-0.5f && isLeft) canRun = true;
+            if(canRun){
+                animator.SetBool("isRun",true);
+                rigid.AddForce( new Vector2(dashSpeed*-1, 0),ForceMode2D.Impulse);
+                if(rigid.velocity.x < -8f) 
+                    rigid.velocity = new Vector2(-8f,rigid.velocity.y);            
+            }else{
+                animator.SetBool("isWalk", true);
+                rigid.AddForce( new Vector2(speed*-1, 0),ForceMode2D.Impulse); 
+                if(rigid.velocity.x < -5f) 
+                    rigid.velocity = new Vector2(-5f,rigid.velocity.y);
+
+            }
+
+            spriteRenderer.flipX=true;
+        }
+        //오른쪽 이동 시 로직
+        else if( Input.GetKey(KeyCode.RightArrow)){
+            if(RunDelay>0 && RunDelay<0.4f && rigid.velocity.x >0.5f && isRight) canRun = true;
+            if(canRun){
+                animator.SetBool("isRun",true);
+                rigid.AddForce( new Vector2(dashSpeed*1, 0),ForceMode2D.Impulse);
+                if(rigid.velocity.x > 8f) 
+                    rigid.velocity = new Vector2(8f,rigid.velocity.y);            
+            }else{
+                animator.SetBool("isWalk", true);
+                rigid.AddForce( new Vector2(speed*1, 0),ForceMode2D.Impulse); 
+                if(rigid.velocity.x > 5f) 
+                    rigid.velocity = new Vector2(5f,rigid.velocity.y);
+
+            }
+
+            spriteRenderer.flipX=false;
+
+        }
+
+        if(rigid.velocity.x == 0){
+            //속력 0시 애니메이션 다끄기 ++++++
+            animator.SetBool("isWalk",false);
+            animator.SetBool("isRun",false);
+            canRun=false;
+        }
+
+        // Debug.Log(rightMove + " " + isRunTime);
+        // if((rightMove==1 || leftMove==1) && isRunTime<=0.6f){
+        //     isRunTime-=Time.deltaTime;
+        // }
+        // if((rightMove==1 ||leftMove==1) && isRunTime >0 && Input.GetKeyDown(KeyCode.RightArrow))canRun=true;
+        // if((rightMove>=2 || leftMove>=2) && isRunTime<0f){
+        //     isRunTime=0.6f;
+        //     canRun=false;
+        //     rightMove=0;
+        //     leftMove=0;
+        // }else if((rightMove>=2 || leftMove>=2)) isRunTime-=Time.deltaTime;
         
+
+
         // if(Input.GetKeyUp(KeyCode.RightArrow)) {checkRun = true; isRunTime=0; animator.SetBool("isRun",false);}
         // if(checkRun) isRunTime += Time.deltaTime;
 
@@ -89,37 +137,6 @@ public class Kirby_Move : MonoBehaviour
         // }
     }
 
-    // void MoveCheck(){
-        
-    //     if((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)) && canRun>=2 ){
-    //         animator.SetBool("isRun",true);
-    //         Debug.Log("달린다");
-    //         canRun=2;
-    //         Move(10f);
-    //         isDash = true;
-    //     }else if((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))){
-    //         animator.SetBool("isWalk", true);  
-    //         Move(4f);
-    //         isDash = false;
-    //     }else{
-    //         Move(0f);
-    //         isDash = false;
-    //     }
 
-    // }
-
-    // void Move(float speed){
-    //     float direction = Input.GetAxisRaw("Horizontal");
-
-    //     rigid.velocity = new Vector2(speed*direction, rigid.velocity.y);
-    //     if(rigid.velocity.x > 0){
-    //         spriteRenderer.flipX=false;
-    //     }else if(rigid.velocity.x <0 ){
-    //         spriteRenderer.flipX=true;
-    //     }else{
-    //         animator.SetBool("isWalk", false);
-    //         animator.SetBool("isRun", false);
-    //     }
-    // }
 
 }
